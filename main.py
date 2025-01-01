3bf37460ef335cc0af6733b841845a45fd9f1195
bryanQeaton <bryaneaton529@gmail.com>
2025-01-01 01:27:25 -0800

    Batch-SGD implemented
    best model found so far implemented, ideal with early stopping
    
    next is activations!

--- main.py
+++ main.py
@@ -16,10 +16,13 @@ y=[
 layers=[
     nn.Model.InputLayer(2),
     nn.Model.Layer(3,nn.relu),
+    nn.Model.Layer(3,nn.relu),
     nn.Model.Layer(2,nn.sigmoid)
 ]
 model=nn.Model(layers)
-model.stochasticGradientDescent(x, y, x, y, nn.mse, .01, 1000)
+model.stochastic_batch_gradient_descent(
+    x, y, x, y,4,
+    nn.mse, .01, 1000)
 
 
 
